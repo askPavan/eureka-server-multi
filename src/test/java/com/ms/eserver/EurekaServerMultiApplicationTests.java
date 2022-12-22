af4cabec944a8710eaf5a8c2a6fcd37e6261e84f
askPavan <pavankumarambraji@gmai.com>
2022-12-22 18:41:57 +0530

    Multiple Eureka/Discovery service Implementation

---
+++ src/test/java/com/ms/eserver/EurekaServerMultiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.eserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerMultiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
